b2776c39b5c55e2c15793d0cff3523b5a37d81ec
vyazmetinov <138450265+vyazmetinov@users.noreply.github.com>
2024-12-14 23:40:15 +0300

    Вроде как рабочий open_cells

---
+++ build/CMakeFiles/sapper.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sapper.dir/lib/table/bombs.cpp.o"
+  "CMakeFiles/sapper.dir/lib/table/bombs.cpp.o.d"
+  "CMakeFiles/sapper.dir/lib/table/table.cpp.o"
+  "CMakeFiles/sapper.dir/lib/table/table.cpp.o.d"
+  "CMakeFiles/sapper.dir/lib/vec/vec2d.cpp.o"
+  "CMakeFiles/sapper.dir/lib/vec/vec2d.cpp.o.d"
+  "CMakeFiles/sapper.dir/src/main.cpp.o"
+  "CMakeFiles/sapper.dir/src/main.cpp.o.d"
+  "sapper"
+  "sapper.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sapper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
